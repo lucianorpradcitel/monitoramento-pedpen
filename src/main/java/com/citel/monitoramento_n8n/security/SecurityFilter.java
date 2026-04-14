3f1c44935eb2a8f3ad7fd756318c463555acfb61
lucianorpradcitel <luciano.junior@citelsoftware.com.br>
2026-04-14 11:22:45 -0300

    Revert "Fix: retornar 401 com mensagem descritiva em vez de 403 silencioso"
    This reverts commit d046b65173c9c44b72fdee06968e1f946cdf8e4d.

--- src/main/java/com/citel/monitoramento_n8n/security/SecurityFilter.java
+++ src/main/java/com/citel/monitoramento_n8n/security/SecurityFilter.java
@@ -27,24 +27,22 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         var token = recuperarToken(request);
 
-        if (token == null) {
-            request.setAttribute("auth_error", "Token de autenticação não informado");
-        } else {
+        if (token != null)
+        {
             var subject = tokenService.validarToken(token);
-            if (subject == null) {
-                request.setAttribute("auth_error", "Token inválido ou expirado");
-            } else {
+            if (subject != null)
+            {
                 var cliente = clienteRepository.findByUserName(subject);
-                if (cliente == null) {
-                    request.setAttribute("auth_error", "Usuário não encontrado");
-                } else {
+                if(cliente != null)
+                {
                     var authentication = new UsernamePasswordAuthenticationToken(cliente, null, cliente.getAuthorities());
                     SecurityContextHolder.getContext().setAuthentication(authentication);
+
                 }
             }
         }
 
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
 
     }
 
